062873cc5b10c19f1bd35c06582ffe478c213504
JawnBlue <brownjon000@gmail.com>
2021-02-07 11:12:57 -0600

    Added PEP 8 Style Format to main.py

--- main.py
+++ main.py
@@ -1,40 +1,42 @@
 import playsound as ps
 import numpy as np
 import re
+import importlib
+import unit1database
 
 
-class Main_Menu:
+class MainMenu:
 
-  def displayWelcomeMessage():
+  def display_welcome_message():
     print("Thank you so much for your willingness to test our program!! This is a beta build of the HowToStudyKorean Listening Program.")
     print("\nWhile a fancy interface undergoes development, we'll be utilizing the Python console for testing Lessons 7-25.")
 
-class Get_User_Input():
+class GetUserInput():
     print("\n-----------------------------------------\n")
 
-    def chooseLesson():
+    def choose_lesson():
       lesson = int(input("Which lesson would you like to practice? [From Lessons 7 to 25]"))
       while  lesson < 7 or lesson > 25 and lesson != 0:
         print("Only lessons Lessons 7 - 25 are available.")
         lesson = int(input("Which lesson would you like to practice?"))
       return(lesson)
 
-    def limitTheNumberOfPracticeQuestions(ProgrammedLesson):
-      numQuestions = len(ProgrammedLesson)
+    def limit_the_number_of_practice_questions(ProgrammedLesson):
+      num_questions = len(ProgrammedLesson)
       txt = "There are {} possible questions to respond to."
-      print(txt.format(numQuestions))
+      print(txt.format(num_questions))
       limitCond = str(input("Would you like to reduce the number of questions to answer? [Y/N]"))
       if limitCond in ['y', 'Y']:
-        newNumQuestions = int(0)
-        while newNumQuestions < 1 or newNumQuestions > numQuestions:
-          newNumQuestions = int(input(""))
+        newnum_questions = int(0)
+        while newnum_questions < 1 or newnum_questions > num_questions:
+          newnum_questions = int(input(""))
       else:
-        newNumQuestions = numQuestions
-      return(newNumQuestions)
+        newnum_questions = num_questions
+      return(newnum_questions)
 
 
-      # There are currently {} amount of questions in Lesson X. Would you like to choose a smaller amount of questions for this session? numQuestions = input("\nHow many questions would you like to answer?")
-    def typeOfQuestions():
+      # There are currently {} amount of questions in Lesson X. Would you like to choose a smaller amount of questions for this session? num_questions = input("\nHow many questions would you like to answer?")
+    def practice_type():
       questionType = str("")
       print("\nFinally, what kind of lesson are you hoping to do? [A or B]\n")
       print("[A] KOR --> KOR -------- Type out the Korean from the audio")
@@ -48,29 +50,34 @@ class Get_User_Input():
         print("Only choose A or B")
       return(questionType)
 
-#    return(chooseLesson, numQuestions, typeOfQuestions)
+#    return(choose_lesson, num_questions, practice_type)
 
-class Practice_Format:
-  def __init__(self, selectLesson, numQuestions, typeOfQuestions):
-    self.selectLesson = selectLesson
-    self.numQuestions = numQuestions
-    self.typeOfQuestions = typeOfQuestions
+class PracticeFormat:
+  def __init__(self, select_lesson, num_questions, practice_type):
+    self.select_lesson = select_lesson
+    self.num_questions = num_questions
+    self.practice_type = practice_type
 
 
-class Lesson_Dictionary:
-  def __init__(self, korean, english, conjugation):
+class LessonDetails:
+  def __init__(cls, korean, english, conjugation):
     self.korean = korean
     self.english = english
     self.conjugation = conjugation
 
+  def retrieve_lesson_from_database(num_lesson):
+    str = "L{}arr"
+    str = str.format(num_lesson)
+    lesson_object =
+
 
 def main():
-  Main_Menu.displayWelcomeMessage()
-  l = Get_User_Input.chooseLesson()
-  print(l)
-  t = Get_User_Input.typeOfQuestions()
+  MainMenu.display_welcome_message()
+  numLesson = GetUserInput.choose_lesson()
+  lessonObject = LessonDetails.retrieve_lesson_from_database(numLesson)
+  t = GetUserInput.practice_type()
   print(t)
 
 
-  
+
 main()
